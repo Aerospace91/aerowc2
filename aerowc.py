60227f3b7a18d8e2df53f842800611095c37e9df
Cory Burmahl <beenters@gmail.com>
2025-07-24 17:48:20 -0700

    Added Lines function

--- aerowc.py
+++ aerowc.py
@@ -6,21 +6,24 @@ def main():
 
     parser.add_argument("filename")
     parser.add_argument("-c", "--bytes", action="store_true", help="Show Byte Count of File")
+    parser.add_argument("-l", "--lines", action="store_true", help="Show number of lines in the file")
 
     args = parser.parse_args()
 
-    with open(args.filename, "rb") as f:
-        data = f.read()
-
     if args.bytes:
-        print(f"{byte_count(data)} {args.filename}")
+        print(f"{byte_count(args.filename)} {args.filename}")
+    elif args.lines:
+        print(f"{line_count(args.filename)} {args.filename}")
         
 
 
-def byte_count(data):
-    return len(data)
+def byte_count(filename):
+    with open(filename, "rb") as f:
+        return len(f.read())
 
-
+def line_count(filename):
+    with open(filename, "r") as f:
+        return sum(1 for line in f)
 
 if __name__ == "__main__":
     main()
