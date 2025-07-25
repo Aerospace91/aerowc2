67099cbde55c30fa9f9b8d92f2325189299b4ef4
Cory Burmahl <beenters@gmail.com>
2025-07-24 17:41:50 -0700

    Added Byte Count Function

--- aerowc.py
+++ aerowc.py
@@ -1,5 +1,26 @@
 import argparse
 
-parser = argparse.ArgumentParser(descripion="Aero's Version of WC")
+def main():
 
-parser.add_argument("-c", "--bytes")
+    parser = argparse.ArgumentParser(description="Aero's Version of WC")
+
+    parser.add_argument("filename")
+    parser.add_argument("-c", "--bytes", action="store_true", help="Show Byte Count of File")
+
+    args = parser.parse_args()
+
+    with open(args.filename, "rb") as f:
+        data = f.read()
+
+    if args.bytes:
+        print(f"{byte_count(data)} {args.filename}")
+        
+
+
+def byte_count(data):
+    return len(data)
+
+
+
+if __name__ == "__main__":
+    main()
